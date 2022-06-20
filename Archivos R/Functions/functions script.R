b61be8d904e50196446beebabe30f0bc382c5710
sdiazuc3m <103251643+sdiazuc3m@users.noreply.github.com>
2022-06-20 21:12:36 +0200

    Update functions script.R

--- Archivos R/Functions/functions script.R
+++ Archivos R/Functions/functions script.R
@@ -40,8 +40,8 @@ getStratum <- function(n,StratumProp){
 # This generates the Population
 genPopulation <- function(n, dis_pop, pop_vect,prob_hidden) {
   # Generates a data frame with the population and the belonging to the Hidden Population
-  enc = data.frame(Poblacion = getPopDis(n,dis_pop,pop_vect))
-  enc = cbind(enc, Poblacion_Oculta = getHiddenPop(n,prob_hidden))
+  enc = data.frame(Population = getPopDis(n,dis_pop,pop_vect))
+  enc = cbind(enc, Hidden_Population = getHiddenPop(n,prob_hidden))
   rownames(enc) <- c(1:n)
   return(enc)
 }
@@ -51,11 +51,11 @@ genPopulation <- function(n, dis_pop, pop_vect,prob_hidden) {
 ######################
 
 matrixHP = function(grafo,Pob){
-  # adjacency matrix of the undirected graph of connections with the Hidden Population
+  # adjacency matrix of the directed graph of connections with the Hidden Population
   ad = as_adj(grafo) # adjacency matrix
   for (j in 1:ncol(ad)) {
     #if (V(grafo)$label[j] == 0){
-    if (Pob$Poblacion_Oculta[j]==0){
+    if (Pob$Hidden_Population[j]==0){
       ad[,j] = 0
     }
   }
@@ -108,7 +108,7 @@ getPoblacionTotal_sinmix = function(N, poblaciones_dis,v_probabilidades,PropHidd
   for (i in 1:N) {
     # En la clase de igraph, [[]] busca vértices adyacentes
     # net_sw[[i]] es un lista con un elemento, la lista con los vértices adyacentes
-    vect_hp[i] = sum(Pob_general[net_sw[[i]][[1]],]$Poblacion_Oculta)
+    vect_hp[i] = sum(Pob_general[net_sw[[i]][[1]],]$Hidden_Population)
     #  binomial de tamaño las conexiones con la población oculta y probabilidad la visibilidad
     vect_hp_vis[i] = rbinom(1,vect_hp[i],prob = visibility_factor)
     vect_reach[i] = length(net_sw[[i]][[1]])
@@ -166,7 +166,7 @@ getPoblacionTotal = function(N, poblaciones_dis,v_probabilidades,PropHiddenPop, 
     # En la clase de igraph, [[]] busca vértices adyacentes
     # net_sw[[i]] es un lista con un elemento, la lista con los vértices adyacentes
     
-    vect_hp[i] = sum(Pob_general[net_sw[[i]][[1]],]$Poblacion_Oculta)
+    vect_hp[i] = sum(Pob_general[net_sw[[i]][[1]],]$Hidden_Population)
     vect_reach[i] = length(net_sw[[i]][[1]])
     #  binomial de tamaño las conexiones con la población oculta y probabilidad la visibilidad
     vect_hp_vis[i] = rbinom(1,vect_hp[i],prob = visibility_factor)
@@ -218,7 +218,7 @@ getData = function(N, dis_populations,prob_vector,PropHiddenPop, dim, nei, p, vi
   
   #N: population size
   #dis_populations: vector with the populations
-  #prob_vector vector con with the population probabilities
+  #prob_vector vector con with the population's probabilities
   #PropHiddenPop: Hidden Population proportion
   #dim: Integer constant, the dimension of the starting lattice.
   #nei: Integer constant, the neighborhood within which the vertices of the lattice will be connected.
@@ -226,60 +226,53 @@ getData = function(N, dis_populations,prob_vector,PropHiddenPop, dim, nei, p, vi
   #memory_factor: the memory factor
   #visibility_factor: the visibility factor
   #memory_factor: es la proporción que vamos a tomar la varianza de la normal que vamos a aplicar al Reach
+  #memory factor: numeric value, Reach divided by the standard deviation of the normal we use to correct the Reach
   #sub_memory_factor: es la proporción que vamos a tomar como varianza de la normal que vamos a aplicar a las subpoblaciones
+  # sub_memory_factor: the subpopulations' visibility divided by the standard deviation of the normals we use to correct the subpopulations' visibility
   
-  Pob_general = genPopulation(N, dis_populations,prob_vector,PropHiddenPop)
+  Population = genPopulation(N, dis_populations,prob_vector,PropHiddenPop)
   
   net_sw = sample_smallworld(dim, N, nei, p, loops = FALSE, multiple = FALSE)
   
-  n_poblaciones = length(dis_populations)-1
-  # Muestra con reemplazamiento de los factores de correcci?n con probabilidades vect_prob_memory_subpob
-  vect_hp = rep(NA,N)       #Vector sin visibility
-  vect_hp_vis = rep(NA,N)   #Vector con visibility aplicado
-  vect_reach = rep(NA,N)    #Vector del número de nodos que se conecta cada nodo
-  vect_reach_re = rep(NA,N) #Vector Reach con error de memoria
+  n_populations = length(dis_populations)-1
+  # initializes the vectors
+  vect_hp = rep(NA,N)       # number of hidden population individuals known for each person
+  vect_hp_vis = rep(NA,N)   # vect_hp applying visibility
+  vect_reach = rep(NA,N)    # the degrees of each individual
+  vect_reach_re = rep(NA,N) # Reach vector applying memory error
   
-  # Creamos la matriz del grafo dirigido de personas que conocen a la poblaci?n oculta
-  Mhp = matrixHP(net_sw,Pob_general)
+  # Matrix representing the directed graph that connects individuals with the people of the Hidden Population they know 
+  Mhp = matrixHP(net_sw,Population)
   Mhp_vis =  apply(Mhp,c(1,2), berHP,p = visibility_factor)
   
   
   for (i in 1:N) {
-    # En la clase de igraph, [[]] busca vértices adyacentes
-    # net_sw[[i]] es un lista con un elemento, la lista con los vértices adyacentes
+    # net_sw[[i]], list with one element, the list of the adjacent vertices to i
     
     vect_hp[i] = sum(Mhp[i,])
     vect_reach[i] = length(net_sw[[i]][[1]])
-    #  binomial de tamaño las conexiones con la población oculta y probabilidad la visibilidad
-    #vect_hp_vis[i] = rbinom(1,vect_hp[i],prob = visibility_factor)
     vect_hp_vis[i] = sum(Mhp_vis[i,])
     
     vect_reach_re[i] = round(rnorm(1, mean = vect_reach[i], sd = memory_factor*vect_reach[i]))
   }
   
-  Pob_general = cbind(Pob_general, Reach = vect_reach)
-  Pob_general = cbind(Pob_general, Reach_memory = vect_reach_re)
-  Pob_general = cbind(Pob_general, HP_total_conocida = vect_hp) 
-  Pob_general = cbind(Pob_general, HP_total_apvis = vect_hp_vis)
+  Population = cbind(Population, Reach = vect_reach)
+  Population = cbind(Population, Reach_memory = vect_reach_re)
+  Population = cbind(Population, HP_total = vect_hp) 
+  Population = cbind(Population, HP_total_apvis = vect_hp_vis)
   
-  for(j in 0:n_poblaciones){
+  for(j in 0:n_populations){
     v_1 = rep(NA,N)
     for(i in 1:N) {
-      vis_pob = sum(Pob_general[net_sw[[i]][[1]],]$Poblacion == j)
+      vis_pob = sum(Population[net_sw[[i]][[1]],]$Population == j)
       # Visibilidad de la poblaci?n j por i aplicando un factor de visibilidad para las subpoblaciones
       v_1[i] = round(rnorm(1, mean = vis_pob, sd = sub_memory_factor*vis_pob))
     }
     
-    Pob_general = cbind(Pob_general,Subpoblacion_total = v_1)
-    names(Pob_general)[dim(Pob_general)[2]] = str_c("KP_total_apvis_",j)
+    Population = cbind(Population,Subpoblacion_total = v_1)
+    names(Population)[dim(Population)[2]] = str_c("KP_total_apvis_",j)
   }
   
-  
-  
-  
-  
-  # A?adimos las etiquetas de la pertenencia a la Hidden Population al grafo
-  #V(net_sw)$label = Pob_general$Poblacion_Oculta
   
   return(list(net_sw, Pob_general, Mhp_vis))
 }
@@ -303,7 +296,7 @@ getDatos_v2 = function(N, poblaciones_dis,v_probabilidades,PropHiddenPop, dim, n
   
   net_sw = sample_smallworld(dim, N, nei, p, loops = FALSE, multiple = FALSE)
   
-  n_poblaciones = length(poblaciones_dis)-1
+  n_populations = length(poblaciones_dis)-1
   # Muestra con reemplazamiento de los factores de correcci?n con probabilidades vect_prob_memory_subpob
   vect_hp = rep(NA,N)       #Vector sin visibility
   vect_hp_vis = rep(NA,N)   #Vector con visibility aplicado
@@ -331,7 +324,7 @@ getDatos_v2 = function(N, poblaciones_dis,v_probabilidades,PropHiddenPop, dim, n
   Pob_general = cbind(Pob_general, HP_total_conocida = vect_hp) 
   Pob_general = cbind(Pob_general, HP_total_apvis = vect_hp_vis)
   
-  for(j in 0:n_poblaciones){
+  for(j in 0:n_populations){
     v_1 = rep(NA,N)
     for(i in 1:N) {
       vis_pob = sum(Pob_general[net_sw[[i]][[1]],]$Poblacion == j)
@@ -374,7 +367,7 @@ getDatos_conEstrat = function(N, poblaciones_dis,v_probabilidades,PropHiddenPop,
   
   net_sw = sample_smallworld(dim, N, nei, p, loops = FALSE, multiple = FALSE)
   
-  n_poblaciones = length(poblaciones_dis)-1
+  n_populations = length(poblaciones_dis)-1
   
   # Vector con la pertenencia a los diferentes estratos
   Estratos = getEstrato(N,StratumProp)
@@ -408,7 +401,7 @@ getDatos_conEstrat = function(N, poblaciones_dis,v_probabilidades,PropHiddenPop,
   Pob_general = cbind(Pob_general, HP_total_conocida = vect_hp) 
   Pob_general = cbind(Pob_general, HP_total_apvis = vect_hp_vis)
   
-  for(j in 0:n_poblaciones){
+  for(j in 0:n_populations){
     v_1 = rep(NA,N)
     for(i in 1:N) {
       vis_pob = sum(Pob_general[net_sw[[i]][[1]],]$Poblacion == j)
