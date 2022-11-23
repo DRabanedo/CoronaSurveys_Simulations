947a35206e55b09f2d949a75d031feef0c3801d6
sdiazuc3m <103251643+sdiazuc3m@users.noreply.github.com>
2022-11-23 11:24:47 +0100

    Simulaciones y corrección

--- Simulaciones pendientes (Uniforme)/Simulation_numbersubpopulations_dnd.R
+++ Simulaciones pendientes (Uniforme)/Simulation_numbersubpopulations_dnd.R
@@ -471,7 +471,7 @@ timer
 
 ####################### Network analysis #######################################
 ###### Links to the hidden population distribution & Degree distribution #######
-plot_name = str_c("Network_numbersubpopulations_", seed_number, ".png")
+plot_name = str_c("Network_numbersubpopulations_", seed, ".png")
 
 png(filename = plot_name,
     width = 1000, height = 1000)
